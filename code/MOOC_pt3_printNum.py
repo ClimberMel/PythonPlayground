0296bf630d4877ff7a3a6da3bbf458835d57e31b
ClimberMel <climbermel@gmail.com>
2024-01-25 14:10:19 -0800

    MOOC solutions

---
+++ code/MOOC_pt3_printNum.py
@@ -0,0 +1,8 @@
+'''Please write a program which prints out all the even numbers between two and thirty, 
+using a loop.  Print each number on a separate line.
+If you want it to include 30, then the stop needs to be 31
+Otherwise it will print 2 to 30 excluding 30'''
+
+# range(start, stop, step)
+for number in range(2, 31, 2):
+    print(number)
