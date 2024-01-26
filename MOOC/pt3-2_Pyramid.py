e96af152823857db09d6b98180dd37665537796f
ClimberMel <climbermel@gmail.com>
2024-01-25 16:30:33 -0800

    More MOOC answers

---
+++ MOOC/pt3-2_Pyramid.py
@@ -0,0 +1,11 @@
+''' Draw a pyramid of *
+    Example provided by MOOC
+'''
+
+n = 10 # number of layers in the pyramid
+row = "*"
+
+while n > 0:
+    print(" " * n + row)
+    row += "**"
+    n -= 1
